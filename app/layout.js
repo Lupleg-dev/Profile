971bf4ac2cad8864eab108869739f8fc1e154ae2
marksikaundi <emmanuelmark117@gmail.com>
2024-12-03 22:42:48 +0200

    chhnged h

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ import Header from "@/components/Header";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Mark Sikaundi - SE profile",
+  title: "Mark Sikaundi - Profile",
   description: "I am a passionate Software Engineer with over 5 years of experience in building web applications. I have a strong background in JavaScript, React, Node.js, and various databases. I am always eager to learn new technologies and tackle challenging projects.",
 };
 
