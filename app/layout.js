6a02cfdd09e44402d3b6263a50602c59716658fa
marksikaundi <emmanuelmark117@gmail.com>
2025-03-06 13:52:06 +0000

    🌙 Add ThemeProvider to RootLayout for theme management

--- app/layout.js
+++ app/layout.js
@@ -2,22 +2,31 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Mark Sikaundi - Profile",
-  description: "I am a passionate Software Engineer with over 5 years of experience in building web applications. I have a strong background in JavaScript, React, Node.js, and various databases. I am always eager to learn new technologies and tackle challenging projects.",
+  description:
+    "I am a passionate Software Engineer with over 5 years of experience in building web applications. I have a strong background in JavaScript, React, Node.js, and various databases. I am always eager to learn new technologies and tackle challenging projects.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header/>
-        {children}
-        <Footer/>
-        </body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Header />
+          {children}
+          <Footer />
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
