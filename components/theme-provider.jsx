6d8bd49e1167f8b47cefb9a7400829149a37e6f0
marksikaundi <emmanuelmark117@gmail.com>
2025-03-06 13:50:13 +0000

    🔧 Fix ThemeProvider component to correctly handle children prop

--- components/theme-provider.jsx
+++ components/theme-provider.jsx
@@ -1,6 +1,6 @@
-"use client"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-export function ThemeProvider(props) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
-}
+"use client";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
+export function ThemeProvider({ children, ...props }) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+}
